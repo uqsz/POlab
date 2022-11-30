152d12203ca573972efe9b67073efaafbf56223b
Łukasz Stępień <uqsz123@gmail.com>
2022-11-30 17:39:26 +0100

    lab7 window

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,5 +1,8 @@
 package agh.ics.oop;
 
+import agh.ics.oop.gui.App;
+import javafx.application.Application;
+
 public class World {
     public static void main(String[] args) {
         MoveDirection[] directions = new OptionsParser().parse(args);
@@ -7,7 +10,7 @@ public class World {
         Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
         IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
-        System.out.println(map);
+        Application.launch(App.class, args);
     }
 }
 
---
+++ src/main/java/agh/ics/oop/gui/App.java
@@ -0,0 +1,11 @@
+package agh.ics.oop.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    public void start(Stage primaryStage){
+        primaryStage.show();
+    }
+
+}
