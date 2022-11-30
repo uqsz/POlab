8cb7d9d71b3bfc374d972ffc8f368c2c73db78ec
Łukasz Stępień <uqsz123@gmail.com>
2022-11-30 18:10:51 +0100

    lab7 window

--- src/main/java/agh/ics/oop/gui/App.java
+++ src/main/java/agh/ics/oop/gui/App.java
@@ -1,11 +1,20 @@
 package agh.ics.oop.gui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class App extends Application{
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) throws Exception{
+        Label label = new Label("Zwierzak");
+        Scene scene = new Scene(label, 400, 400);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
 }
